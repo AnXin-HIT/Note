846e3720265d86716089d79fec5f32f451da4778
AnXin-HIT <x.an.hit@outlook.com>
2025-01-11 18:26:05 +0800

    提交了第2讲（C语言数据类型和变量）第一部分的课程笔记

---
+++ 比特就业课/课程笔记/第2讲_C语言数据类型和变量/Code/C_Language_Data_Types_and_Variables/C_Language_Data_Types_and_Variables/Test.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdbool.h>
+
+int m = 1000;
+
+int main()
+{
+	int a = -10;
+	printf("%d\n", -10);	// -10
+	printf("%d\n", -a);		// 10
+	printf("%d\n", 10);		// 10
+
+	return 0;
+}
